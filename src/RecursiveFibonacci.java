ab73a72c4e4e34ab648b4645228a83efdac76d91
atanaspunev <punev90@gmail.com>
2022-01-14 12:44:18 +0200

    Added solutions of problem Recursive Fibonacci

--- src/RecursiveFibonacci.java
+++ src/RecursiveFibonacci.java
@@ -4,6 +4,22 @@ public class RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
 
+        if (n == 0) {
+            System.out.println("0");
+            return;
+        }
+
+        long result = fib(n);
+
+        System.out.println(result);
+    }
+
+    private static long fib(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
